5950cb1ad2fa1813b5d2d1a5de446d8ee166104a
Asees1407 <75072372+Asees1407@users.noreply.github.com>
2022-01-22 18:31:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var firstname="Asees ";
+var lastname="Kaur";
+var fullname= firstname.concat(lastname);
+console.log(fullname);
+var number1=13;
+console.log(number1)
+var number2=7;
+console.log(number2)
+var number3 = 40
+console.log(number3)
+var add = number1+number2;
+console.log(add);
+var sub = number1-number2;
+console.log(sub);
+var mul = number1*number2;
+console.log(mul);
+var div = number1/number2;
+console.log(div);
+var sqrt = Math.sqrt(number3);
+console.log(sqrt);
+var pow = Math.pow(13, 7)
+console.log(pow);
